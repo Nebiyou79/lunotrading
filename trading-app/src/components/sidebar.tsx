4eddcc30838c30733096a3f16f8c627bfb57f97d
Nebiyou Girma <104990722+Nebiyou79@users.noreply.github.com>
2024-10-23 12:49:09 +0300

    login and sign-up

--- trading-app/src/components/sidebar.tsx
+++ trading-app/src/components/sidebar.tsx
@@ -1,33 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import { FaUser, FaIdCard, FaEdit, FaHistory, FaHeadset } from 'react-icons/fa';
-import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Sidebar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  // Toggle sidebar for mobile view
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
 
   return (
     <div>
-      {/* Mobile View (Hamburger Menu) */}
       <div className="md:hidden bg-gray-800 p-2 flex justify-between items-center">
-        <div className="text-white text-xl">Luno Trading</div>
-        <GiHamburgerMenu
-          className="text-white text-3xl cursor-pointer"
-          onClick={toggleSidebar}
-        />
-      </div>
-
-      {/* Mobile Sidebar Overlay */}
-      <div
-        className={`fixed inset-0 z-20 transition-transform transform ${
-          isOpen ? 'translate-x-0' : '-translate-x-full'
-        } bg-gray-800 md:hidden`}
-      >
         <div className="p-4 space-y-6">
           <Link href="/dashboard">
             <FaUser className="text-white text-2xl" />
